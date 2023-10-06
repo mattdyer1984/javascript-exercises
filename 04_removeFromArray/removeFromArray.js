482214b47c1d09aba33c25e8167fb168ad78114f
Apple <apple@matts-pro.home>
2023-10-06 21:05:17 +0100

    first 5 exercises completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, a = "", b = "", c = "", d = "") {
+  let i = arr.indexOf(a);
+  if (i != -1) {
+    arr.splice(i, 1);
+  }
+  let j = arr.indexOf(b);
+  if (j != -1) {
+    arr.splice(j, 1);
+  }
+  let k = arr.indexOf(c);
+  if (k != -1) {
+    arr.splice(k, 1);
+  }
+  let l = arr.indexOf(d);
+  if (l != -1) {
+    arr.splice(l, 1);
+  }
+  return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number" || a < 0 || b < 0) {
+    return "ERROR";
+  }
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    return sum;
+  } else
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  return sum;
 };
 
 // Do not edit below this line
