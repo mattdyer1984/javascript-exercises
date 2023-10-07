eb6b87879f50a1e79c05f36e8ca8ad18a582bb98
Apple <apple@matts-pro.home>
2023-10-07 09:43:10 +0100

    ex9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase(); // Convert the string to lowercase
+    let regex = /[a-z0-9]/g;
+    let answer = true;
+    let matches = str.match(regex);
+    for (let i = 0; i < Math.floor(matches.length / 2); i++) {
+        if (matches[i] !== matches[matches.length - i - 1]) {
+            answer = false;
+            break;
+        }
+    }
+    return answer;
 };
 
 // Do not edit below this line
