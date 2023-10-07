eab324965a574469e0efa61e5f1270d44ac92341
Apple <apple@Matts-Pro.local>
2023-10-07 10:46:50 +0100

    ex10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+function fibonacci(num) {
+    num = parseInt(num, 10)
+    if(num < 0 ){
+        return 'OOPS'
+    }
+    let fibonacci = []
+    for(let i = 1; i <= num; i ++){
+        if(i > 2){
+        fibonacci.push(fibonacci[i-3] + fibonacci[i-2])
+    } else {
+        fibonacci.push(1)
+    }
+} return fibonacci[num-1]
 };
 
 // Do not edit below this line
