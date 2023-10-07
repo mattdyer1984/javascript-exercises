d9e774a24f6c91527b9b7e183e2d2693e7dd9dea
Apple <apple@Matts-Pro.local>
2023-10-07 11:40:15 +0100

    exercises completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+function findTheOldest(people) {
+   
+    for(let i = 0; i < people.length; i++) {
+        if(people[i].yearOfDeath) {
+            people[i].age = people[i].yearOfDeath - people[i].yearOfBirth
+        } else {
+            people[i].age = 2023 - people[i].yearOfBirth
+        }
+    }
+    let result = [people[0]]
+    for(let i = 0; i < people.length; i++) {
+        if(people[i].age > result[0].age){
+            result.unshift(people[i])
+        }
+    }
+    return result[0]
 
 };
 
