05c1da5198faa3b90f4dda3de733c378e7ae0510
Apple <apple@matts-pro.home>
2023-10-07 08:36:18 +0100

    ex8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
+const add = function(a, b) {
+  return  a + b
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+	return a - b
+};
+
+const sum = function(arr) {
+  let result = 0
+  arr.forEach(element => {
+    result += element
+  });
+  return result
 	
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let result = 1
+  arr.forEach(element => {
+    result *= element
+  });
+  return result
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+  let result = a
+	for(let i = 1; i < b; i++) {
+    result *= a
+  }
+  return result
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num
+  if(num == 0) {
+    result = 1 
+    } else { for(let i = 1; i < num; i++) {
+    result *= num - i
+  }}
+  return result
 };
 
 // Do not edit below this line
